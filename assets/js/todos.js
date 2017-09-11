1e5ce302d3c7d53ef58a86271bf09d1f7996d01e
Robert Hardt <rob@Roberts-MacBook-Pro.local>
2017-09-11 12:17:05 -0400

    add strikethrough on click

---
+++ assets/js/todos.js
@@ -0,0 +1,16 @@
+// check off specific todos by clicking
+$("li").click(function(){
+  //if li is gray
+  if($(this).css("color") === "rgb(128, 128, 128)") {
+    $(this).css({
+      color: "black",
+      textDecoration: "none"
+    });
+  }
+  else {
+    $(this).css({
+      color: "gray",
+      textDecoration: "line-through"
+    });
+  }
+})
