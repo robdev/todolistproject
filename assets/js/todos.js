afab2b0179e74dab5d523df8978294bea5fc30b2
Robert Hardt <rob@Roberts-MacBook-Pro.local>
2017-09-11 12:39:34 -0400

    remove/add list items

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,16 +1,21 @@
 // check off specific todos by clicking
-$("li").click(function(){
-  //if li is gray
-  if($(this).css("color") === "rgb(128, 128, 128)") {
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
-  }
-  else {
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "li span", function(evnt){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  evnt.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(evnt){
+  if(evnt.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> "+todoText+"</li>")
   }
 })
